326eb1f7bf010d762fc84ab2830343af392c194a
Web4 <167559384+Web4application@users.noreply.github.com>
2025-04-07 21:19:13 -0400

    Update clone.sh

--- git/clone.sh
+++ git/clone.sh
@@ -1,1 +1,22 @@
-https://github.com/Kubucoinincs/web4.git
+#!/bin/bash
+# A script to clone Git repositories
+
+# List of repositories to clone
+REPOS=(
+    "https://github.com/Kubucoinincs/web4.git"
+    "https://github.com/another/repository.git"
+)
+
+# Target directory to store cloned repositories
+TARGET_DIR="./cloned_repositories"
+
+# Create the target directory if it doesn't exist
+mkdir -p $TARGET_DIR
+
+# Loop through the list of repositories and clone them
+for REPO in "${REPOS[@]}"; do
+    echo "Cloning $REPO into $TARGET_DIR..."
+    git clone $REPO $TARGET_DIR/$(basename $REPO .git)
+done
+
+echo "All repositories have been cloned."
